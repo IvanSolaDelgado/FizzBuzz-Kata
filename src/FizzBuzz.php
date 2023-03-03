b6d68bd9f24cd701ae364a2e3938d2e6690d7423
Iván Sola Delgado <ivan@MacBook-Air-de-Ivan.local>
2023-03-03 09:35:18 +0100

    Added new functionality fizz

---
+++ src/FizzBuzz.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Deg540\PHPTestingBoilerplate;
+class FizzBuzz
+{
+    public function tryFizzBuzz(int $numberToConvert): int|string
+    {
+        if($numberToConvert%3 === 0){
+            return "fizz";
+        }
+    }
+}
---
+++ tests/FizzBuzzTest.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Deg540\PHPTestingBoilerplate\Test;
+
+use Deg540\PHPTestingBoilerplate\FizzBuzz;
+use PHPUnit\Framework\TestCase;
+
+final class FizzBuzzTest extends TestCase
+{
+    /**
+     * @test
+     */
+
+    public function shouldSaysFizzWhenNumberMultipleOfThree()
+    {
+        $fizzbuzz = new FizzBuzz();
+
+        $result = $fizzbuzz->tryFizzBuzz(9);
+
+        $this->assertEquals("fizz",$result);
+    }
+
+}
